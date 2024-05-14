2f61818e664abac6cb5543ec3b7009e3922ef477
hasinajy <jyhasina@gmail.com>
2024-05-14 12:50:14 +0300

    fix: Delete library in the test directory first before copying

--- packaging/packaging.bat
+++ packaging/packaging.bat
@@ -10,6 +10,7 @@ rem Create the archive inside the target directory
 jar -cvf %target_dir%/winter.jar -C %bin_dir% .
 
 rem Send the library to the target directory
+del %test_dir%\winter.jar
 echo D | xcopy /q/s/y "..\lib\winter.jar" %test_dir%
 
 echo Created winter.jar from %bin_dir%
