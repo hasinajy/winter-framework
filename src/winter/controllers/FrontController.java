029a4e4f5aefa94a2c8303162b19419170d8f267
hasinajy <jyhasina@gmail.com>
2024-05-14 10:13:23 +0300

    feat: Add variable isScanned to check if the controllers have been scanned

--- src/winter/controllers/FrontController.java
+++ src/winter/controllers/FrontController.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class FrontController extends HttpServlet {
+    private boolean isScanned = false;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
