54fc2014b43ec03746448457042adbdd36286165
hasinajy <jyhasina@gmail.com>
2024-05-14 10:58:29 +0300

    style: Format document

--- src/winter/controllers/FrontController.java
+++ src/winter/controllers/FrontController.java
@@ -17,57 +17,58 @@ import jakarta.servlet.http.HttpServletResponse;
 import winter.annotations.Controller;
 
 public class FrontController extends HttpServlet {
-    private boolean isScanned = false;
-    private ArrayList<String> controllers = null;
+  private boolean isScanned = false;
+  private ArrayList<String> controllers = null;
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    processRequest(req, resp);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    processRequest(req, resp);
+  }
+
+  private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String requestURL = req.getRequestURL().toString();
+
+    PrintWriter out = resp.getWriter();
+    out.println("Request URL: " + requestURL);
+  }
+
+  private void scanControllers(ServletContext servletContext)
+      throws URISyntaxException, IOException, ClassNotFoundException {
+    if (isScanned) {
+      return;
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        processRequest(req, resp);
+    String controllersPackage = servletContext.getInitParameter("ControllersPackage");
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    Enumeration<URL> resources = classLoader.getResources(controllersPackage.replace(".", "/"));
+
+    while (resources.hasMoreElements()) {
+      URL resource = resources.nextElement();
+      scanControllers(resource, controllersPackage);
+    }
+  }
+
+  private void scanControllers(URL directory, String packageName)
+      throws URISyntaxException, IOException, ClassNotFoundException {
+    if (!directory.toURI().toString().endsWith("/")) {
+      packageName += ".";
     }
 
-    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String requestURL = req.getRequestURL().toString();
-
-        PrintWriter out = resp.getWriter();
-        out.println("Request URL: " + requestURL);
-    }
-
-    private void scanControllers(ServletContext servletContext) throws URISyntaxException, IOException, ClassNotFoundException {
-        if (isScanned) {
-            return;
+    for (String fileName : listFiles(directory)) {
+      if (fileName.endsWith(".class")) {
+        String className = packageName + fileName.substring(0, fileName.length() - 6);
+        Class<?> clazz = Class.forName(className);
+        if (clazz.isAnnotationPresent(Controller.class)) {
+          this.controllers.add(className);
         }
-
-        String controllersPackage = servletContext.getInitParameter("ControllersPackage");
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Enumeration<URL> resources = classLoader.getResources(controllersPackage.replace(".", "/"));
-
-        while(resources.hasMoreElements()) {
-          URL resource = resources.nextElement();
-          scanControllers(resource, controllersPackage);
-        }
+      }
     }
-
-        private void scanControllers(URL directory, String packageName)
-            throws URISyntaxException, IOException, ClassNotFoundException {
-        if (!directory.toURI().toString().endsWith("/")) {
-            packageName += ".";
-        }
-
-        for (String fileName : listFiles(directory)) {
-            if (fileName.endsWith(".class")) {
-                String className = packageName + fileName.substring(0, fileName.length() - 6);
-                Class<?> clazz = Class.forName(className);
-                if (clazz.isAnnotationPresent(Controller.class)) {
-                    this.controllers.add(className);
-                }
-            }
-        }
-    }
+  }
 
   private static ArrayList<String> listFiles(URL directory) throws IOException {
     ArrayList<String> fileNames = new ArrayList<>();
