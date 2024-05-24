2b6c47a9854faff0f1ae44f7eb98367a1e45adaa
hasinajy <jyhasina@gmail.com>
2024-05-24 13:59:25 +0300

    refactor: Create utility class 'AnnotationScanner.java' to contain scanControllers(...) methods

--- src/winter/controllers/FrontController.java
+++ src/winter/controllers/FrontController.java
@@ -2,11 +2,6 @@ package winter.controllers;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Method;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.HashMap;
 
 import jakarta.servlet.ServletContext;
@@ -15,11 +10,11 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import winter.annotations.*;
 import winter.data.Mapping;
-import winter.utils.DirectoryUtil;
+import winter.utils.AnnotationScanner;
 
 public class FrontController extends HttpServlet {
+  
   private HashMap<String, Mapping> URLMappings = new HashMap<>();
 
   @Override
@@ -27,7 +22,7 @@ public class FrontController extends HttpServlet {
     ServletContext servletContext = getServletContext();
 
     try {
-      scanControllers(servletContext);
+      this.URLMappings = AnnotationScanner.scanControllers(servletContext);
     } catch (Exception e) {
       System.err.println(e.getMessage());
     }
@@ -61,55 +56,4 @@ public class FrontController extends HttpServlet {
     }
   }
 
-  private void scanControllers(ServletContext servletContext)
-      throws URISyntaxException, IOException, ClassNotFoundException {
-    String controllersPackage = servletContext.getInitParameter("ControllersPackage");
-    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    Enumeration<URL> resources = classLoader.getResources(controllersPackage.replace(".", "/"));
-
-    while (resources.hasMoreElements()) {
-      URL resource = resources.nextElement();
-      scanControllers(resource, controllersPackage);
-    }
-  }
-
-  @SuppressWarnings("deprecation")
-  private void scanControllers(URL directory, String packageName)
-      throws URISyntaxException, IOException, ClassNotFoundException {
-    if (!packageName.endsWith(".")) {
-      packageName += ".";
-    }
-
-    for (String fileName : DirectoryUtil.listFiles(directory)) {
-      if (fileName.endsWith(".class")) {
-        String className = packageName + fileName.substring(0, fileName.length() - 6);
-        Class<?> clazz = Class.forName(className);
-
-        if (clazz.isAnnotationPresent(Controller.class)) {
-          Method[] methods = clazz.getMethods();
-
-          for (Method method: methods) {
-            if (method.isAnnotationPresent(GetMapping.class)) {
-              String sURL = method.getAnnotation(GetMapping.class).value();
-              String sMethod = method.getName();
-
-              this.URLMappings.put(sURL, new Mapping(className, sMethod));
-            }
-          }
-        }
-      } else {
-        URL potentialSubDirURL = new URL(directory.toString() + "/" + fileName);
-
-        try {
-          URI subDirURI = potentialSubDirURL.toURI();
-
-          if (subDirURI.getScheme() != null && subDirURI.getPath() != null) {
-            scanControllers(potentialSubDirURL, packageName + fileName + ".");
-          }
-        } catch (URISyntaxException e) {
-          throw e;
-        }
-      }
-    }
-  }
 }
