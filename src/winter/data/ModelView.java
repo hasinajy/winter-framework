f2eba5560df1c941e003eb417148fd3aacd56309
hasinajy <jyhasina@gmail.com>
2024-05-31 10:57:40 +0300

    feat: Create class 'ModelView'

---
+++ src/winter/data/ModelView.java
@@ -0,0 +1,35 @@
+package winter.data;
+
+import java.util.HashMap;
+
+public class ModelView {
+
+    private String jspUrl;
+    private HashMap<String, Object> data;
+
+    // Constructors
+    public ModelView() {
+    }
+
+    public ModelView(String jspUrl) {
+        this.setJspUrl(jspUrl);
+    }
+
+    // Getters & Setters
+    public String getJspUrl() {
+        return jspUrl;
+    }
+
+    public void setJspUrl(String jspUrl) {
+        this.jspUrl = jspUrl;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+
+}
