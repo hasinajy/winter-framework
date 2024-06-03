17407214048ba63f35aeb636635f834218425cad
hasinajy <jyhasina@gmail.com>
2024-06-03 23:10:20 +0300

    refactor: Add Utility class as a superclass for all utilities

---
+++ src/winter/utils/Utility.java
@@ -0,0 +1,9 @@
+package winter.utils;
+
+public class Utility {
+    
+    protected Utility() throws IllegalStateException {
+        throw new IllegalStateException("Cannot instantiate a utility class.");
+    }
+
+}
