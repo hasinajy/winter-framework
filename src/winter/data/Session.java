5109e9923f8557daf0a3fd9b954eb291985ce4f6
hasinajy <jyhasina@gmail.com>
2024-07-18 21:52:28 +0300

    feat: Add Session class

---
+++ src/winter/data/Session.java
@@ -0,0 +1,23 @@
+package winter.data;
+
+import jakarta.servlet.http.HttpSession;
+
+public class Session {
+    private HttpSession httpSession;
+
+    /* ------------------------------ Constructors ------------------------------ */
+    public Session() {}
+
+    public Session(HttpSession httpSession) {
+        this.setHttpSession(httpSession);
+    }
+
+    /* --------------------------- Getters and setters -------------------------- */
+    public HttpSession getHttpSession() {
+        return httpSession;
+    }
+
+    public void setHttpSession(HttpSession httpSession) {
+        this.httpSession = httpSession;
+    }
+}
