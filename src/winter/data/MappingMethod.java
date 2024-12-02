110b618fb361306d935eefeb87ed734ed8e4abd3
hasinajy <jyhasina@gmail.com>
2024-12-02 20:10:56 +0300

    feat: Add MappingMethod

---
+++ src/winter/data/MappingMethod.java
@@ -0,0 +1,54 @@
+package winter.data;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+public class MappingMethod {
+
+    private Method method;
+    private RequestVerb verb;
+
+    /* ------------------------------ Constructors ------------------------------ */
+    public MappingMethod() {
+    }
+
+    public MappingMethod(Method method, RequestVerb verb) {
+        this.setMethod(method);
+        this.setVerb(verb);
+    }
+
+    /* --------------------------- Getters and setters -------------------------- */
+    public Method getMethod() {
+        return method;
+    }
+
+    public void setMethod(Method method) {
+        this.method = method;
+    }
+
+    public RequestVerb getVerb() {
+        return verb;
+    }
+
+    public void setVerb(RequestVerb verb) {
+        this.verb = verb;
+    }
+
+    /* --------------------------------- Methods -------------------------------- */
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof MappingMethod)) {
+            return false;
+        }
+
+        MappingMethod toCompare = (MappingMethod) obj;
+        return this.getMethod().getName().equalsIgnoreCase(toCompare.getMethod().getName())
+                && this.getVerb() == toCompare.getVerb();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this);
+    }
+
+}
