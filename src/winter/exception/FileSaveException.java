80693a3d1ed091ef981527499faef7356094243c
hasinajy <jyhasina@gmail.com>
2025-02-24 23:09:27 +0300

    feat: Create FileSaveException

---
+++ src/winter/exception/FileSaveException.java
@@ -0,0 +1,11 @@
+package winter.exception;
+
+public class FileSaveException extends Exception {
+    public FileSaveException(String message) {
+        super(message);
+    }
+
+    public FileSaveException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
