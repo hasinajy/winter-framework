93ad9523ddca063f9fe2a20fae963ef924c75a47
hasinajy <jyhasina@gmail.com>
2025-03-03 22:51:41 +0300

    docs: Add Javadoc to AnnotationNotFoundException

--- src/winter/data/exception/annotation/AnnotationNotFoundException.java
+++ src/winter/data/exception/annotation/AnnotationNotFoundException.java
@@ -1,10 +1,38 @@
 package winter.data.exception.annotation;
 
+/**
+ * Indicates that a required annotation was not found in the Winter framework.
+ * <p>
+ * This exception is thrown when an expected annotation (e.g.,
+ * {@link winter.data.annotation.http.RequestParam})
+ * is missing from a class, method, field, or parameter during processing, such
+ * as by
+ * {@link winter.service.ControllerHandler} or
+ * {@link winter.data.client.FormData}.
+ * </p>
+ *
+ * @author Hasina JY
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class AnnotationNotFoundException extends Exception {
+
+    /**
+     * Constructs an exception with a message describing the missing annotation.
+     *
+     * @param msg the detail message explaining the error
+     */
     public AnnotationNotFoundException(String msg) {
         super(msg);
     }
 
+    /**
+     * Constructs an exception with a message and a cause for the missing
+     * annotation.
+     *
+     * @param msg   the detail message explaining the error
+     * @param cause the underlying cause of the exception
+     */
     public AnnotationNotFoundException(String msg, Throwable cause) {
         super(msg, cause);
     }
