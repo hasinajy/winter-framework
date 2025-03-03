2d79b55b292f2b3f251bb50ea0fbbcac76e63c66
hasinajy <jyhasina@gmail.com>
2025-03-03 22:54:41 +0300

    docs: Add Javadoc to MappingNotFoundException

--- src/winter/data/exception/client/MappingNotFoundException.java
+++ src/winter/data/exception/client/MappingNotFoundException.java
@@ -1,10 +1,33 @@
 package winter.data.exception.client;
 
+/**
+ * Indicates that a requested URL mapping was not found in the Winter framework.
+ * <p>
+ * This exception is thrown when a client request targets a URL that has no
+ * corresponding
+ * controller method mapping, handled by
+ * {@link winter.service.ExceptionHandler}
+ * to return a 404 response.
+ * </p>
+ *
+ * @author Hasina JY
+ * @version 1.0.0
+ * @since 1.0.0
+ */
 public class MappingNotFoundException extends Exception {
+
+    /**
+     * Constructs a new MappingNotFoundException with no detail message.
+     */
     public MappingNotFoundException() {
         super();
     }
 
+    /**
+     * Constructs a new MappingNotFoundException with the specified detail message.
+     *
+     * @param message the detail message explaining why the mapping was not found
+     */
     public MappingNotFoundException(String message) {
         super(message);
     }
